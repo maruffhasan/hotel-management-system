e49a45d99ff9913e5ea1e0cd6eb1bcc4411913c3
Junayed Islam Tamim <104343539+Dru308@users.noreply.github.com>
2025-07-08 21:08:19 +0600

    no more hardcoded date

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -12,9 +12,20 @@ export default function Home() {
 
   useEffect(() => {
     // Just fetch all rooms with fixed dates for preview
+
+    const today = new Date();
+    const checkIn = today.toISOString().split("T")[0]; // YYYY-MM-DD
+
+    const checkOutDate = new Date(today);
+    checkOutDate.setDate(checkOutDate.getDate() + 1); // 1 day later
+    const checkOut = checkOutDate.toISOString().split("T")[0];
+
     getRooms({
-      check_in: "2025-06-05",
-      check_out: "2025-07-06"
+      // check_in: "2025-06-05",
+      // check_out: "2025-07-06"
+
+      check_in: checkIn,
+      check_out: checkOut
     })
     .then(setRooms)
     .finally(() => setLoading(false));
