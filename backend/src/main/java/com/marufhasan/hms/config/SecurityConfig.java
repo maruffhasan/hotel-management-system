b6f8cf94d74270e36f7c6a24dd17a051bb4e90f5
Maruf Hasan <marufhasan6174@gmail.com>
2025-08-01 00:05:47 +0600

    ...

--- backend/src/main/java/com/marufhasan/hms/config/SecurityConfig.java
+++ backend/src/main/java/com/marufhasan/hms/config/SecurityConfig.java
@@ -75,7 +75,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:3000")); // frontend origin
+        config.setAllowedOrigins(List.of("*")); // frontend origin
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
